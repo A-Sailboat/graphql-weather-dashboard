6abf230bdf1dbab71622eca3bec7c60ba6954e39
Henry <henry.proctor.hp@gmail.com>
2024-08-12 04:24:29 -0700

    enabled cors

--- weather-frontend/src/ApolloClient.js
+++ weather-frontend/src/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://ec2-18-144-174-123.us-west-1.compute.amazonaws.com:4000/graphql",
   cache: new InMemoryCache(),
 });
 
